8afd9e18f2f85fc3056376a8609df2a0582211bf
Trinh Le <ltqa1202@g.ucla.edu>
2023-05-31 16:19:04 -0700

    added interactive map

--- js/init.js
+++ js/init.js
@@ -1,11 +1,14 @@
 // declare variables
 let mapOptions = { 'center': [34.0709, -118.444], 'zoom': 10 }
+    // use the variables
+const dataUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRCF24Aalv8tZ3neF_4LZQ21KLokn5jtZgpc9-wiAuDhT1_LXNYtxKRtsM-eo0UAzhGUHqQvJCgCNmI/pub?output=csv"
+const boundaryLayer = "data/ca_zipcodes.geojson"
+    // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    //     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    // }).addTo(map);
 
-// use the variables
-
-// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-// }).addTo(map);
+let currentLayer
+let zipcodes = []
 
 let pos = L.featureGroup();
 let neg = L.featureGroup();
@@ -26,82 +29,137 @@ let circleOptions = {
     fillOpacity: 0.8
 };
 
-
-const dataUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRCF24Aalv8tZ3neF_4LZQ21KLokn5jtZgpc9-wiAuDhT1_LXNYtxKRtsM-eo0UAzhGUHqQvJCgCNmI/pub?output=csv"
 const map = L.map('the_map').setView(mapOptions.center, mapOptions.zoom);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+let Jawg_Light = L.tileLayer('https://{s}.tile.jawg.io/jawg-light/{z}/{x}/{y}{r}.png?access-token={accessToken}', {
+    attribution: '<a href="http://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    minZoom: 0,
+    maxZoom: 22,
+    subdomains: 'abcd',
+    accessToken: 'FkWnkf1e22dnL71CnkeDRnZeEZRyPNd6DqNr2frT4o5zPMcnKvgfcgG2gQCNjnR7'
+});
+
+Jawg_Light.addTo(map)
+
+// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// }).addTo(map);
 
 L.control.layers(null, layers).addTo(map);
 
+
+
 // create a function to add markers
-function addMarker(data) {
-    let location = data['What is the zip code of your primary home?']
+function addRegion(data) {
+    let zipcode = data['What is the zip code of your primary home?']
     console.log(location, data.lat, data.lng)
-        // L.marker([data.lat, data.lng]).addTo(map).bindPopup(`<h2>${location}</h2> <h3>${(data['Do you like Anime/Manga?'] == "Yes") ? "Watch Anime" : "Do not watch Anime"}</h3>`)
-        // createButtons(data.lat, data.lng, location)
-        // return location
     let experience = data['Overall, what would you rate your work life balance?']
-    let content
-    if (experience.includes("Positive")) {
-        console.log("positive")
-    } else if (experience.includes("Negative")) {
-        content = "Hello"
-        console.log("negative")
-            // neg.addLayer(L.circleMarker([data.lat, data.lng], circleOptions).bindPopup(content))
-        L.marker([data.lat, data.lng]).addTo(map).bindPopup(content)
-    } else {
-        console.log("neutral")
+    let content = "Hello"
+
+    // if (experience.includes("Positive")) {
+    //     console.log("positive")
+    //     L.marker([data.lat, data.lng]).addTo(map).bindPopup(content)
+    // } else if (experience.includes("Negative")) {
+    //     console.log("negative")
+    //         // neg.addLayer(L.circleMarker([data.lat, data.lng], circleOptions).bindPopup(content))
+    //     L.marker([data.lat, data.lng]).addTo(map).bindPopup(content)
+    // } else if (experience.includes("Neutral")) {
+    //     console.log("neutral")
+    //     L.marker([data.lat, data.lng]).addTo(map).bindPopup(content)
+    // }
+    return zipcode
+}
+
+function highlightFeature(e, zipCode) {
+    let layer = e.target;
+    let fillColor = layer.options.fillColor;
+    let count = 0;
+    for (let i = 0; i < zipcodes.length; i++) {
+        if (zipcodes[i] == zipCode) {
+            count++;
+        }
     }
-    // let caregiver = (data['Are you the primary caregiver of a dependent in your household?'] == "Yes") ? true : false;
-    // if (caregiver)
-    // {
-    //     if (data[])
-    // }
-    // else
-    // {
+    let popupMessage = `🏠: ${zipCode}
+                        <br>
+                        Total Responses: ${count}`
+        // Check if the fill color is not grey
+    if (fillColor !== "#efefef") {
+        layer.bindPopup(popupMessage).openPopup();
+        layer.setStyle({
+            weight: 5,
+            color: "#666",
+            dashArray: '',
+            fillOpacity: 0.7
+        });
+    }
 
-    // }
-    return location
+    layer.bringToFront();
+}
 
+function resetHighlight(e) {
+    currentLayer.resetStyle(e.target);
+}
+
+function zoomToFeature(e) {
+    map.fitBounds(e.target.getBounds());
 }
 
 function onEachFeature(feature, layer) {
-    console.log(feature.properties)
-    if (feature.properties.values) {
-        let count = feature.properties.values.length
-        let targetRegion = layer.feature.properties.region
-        console.log(count) // see what the count is on click
-        let text = count.toString() // convert it to a string
-        layer.bindPopup(targetRegion + ': ' + text + ' Responses'); //bind the pop up to the number
-    }
-
+    let zipCode = feature.properties.zcta;
+    layer.on("mouseover", function(e) {
+        highlightFeature(e, zipCode);
+    });
     layer.on({
-        mouseover: highlightFeature,
+        // mouseover: highlightFeature(zipCode),
         mouseout: resetHighlight,
-        click: populateSidebar
+        click: zoomToFeature
     });
 }
 
-// function createButtons(lat, lng, title, content) {
-//     const newButton = document.createElement("button"); // adds a new button
-//     newButton.id = "button" + title; // gives the button a unique id
-//     newButton.innerHTML = title; // gives the button a title
-//     newButton.setAttribute("lat", lat); // sets the latitude 
-//     newButton.setAttribute("lng", lng); // sets the longitude 
-//     newButton.addEventListener('click', function() {
-//         map.flyTo([lat, lng]); //this is the flyTo from Leaflet
-//         const popup = L.popup() // creates a popup instance
-//             .setLatLng([lat, lng]) // sets the popup location
-//             .setContent(content) // sets the popup content
-//             .openOn(map); // opens the popup on the map
-//         window.scrollTo({ top: 80, behavior: "smooth" });
-//     })
-//     const spaceForButtons = document.getElementById('placeForButtons')
-//     spaceForButtons.appendChild(newButton); //this adds the button to our page.
-// }
+function getColor() {
+    var r = Math.floor(Math.random() * 255);
+    var g = Math.floor(Math.random() * 255);
+    var b = Math.floor(Math.random() * 255);
+    return "rgb(" + r + " ," + g + "," + b + ")";
+}
+
+function getBoundary(layer, zipcodes) {
+    fetch(layer)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+
+            //set the boundary to data
+
+            console.log("Current data: ", data)
+                // here is the geoJson of the `collected` result:
+            function style(feature) {
+                let zipCode = feature.properties.zcta;
+
+                // Check if the current zip code is in the desired set
+                if (zipcodes.includes(zipCode)) {
+                    return {
+                        fillColor: getColor(),
+                        weight: 2,
+                        opacity: 1,
+                        color: 'white',
+                        dashArray: '3',
+                        fillOpacity: 0.7
+                    };
+                } else {
+                    return {
+                        fillColor: "#efefef",
+                        opacity: 0
+                    };
+                }
+            }
+            currentLayer = L.geoJSON(data, {
+                style: style,
+                onEachFeature: onEachFeature
+            }).addTo(map)
+        })
+}
 
 function loadData(url) {
     Papa.parse(url, {
@@ -113,15 +171,18 @@ function loadData(url) {
 
 function processData(results) {
     console.log(results)
+
     results.data.forEach(data => {
-        console.log(data)
-        addMarker(data)
+        console.log("data object: ", data)
+        zipcodes.push(addRegion(data))
     })
     pos.addTo(map) // add our layers after markers have been made
     neg.addTo(map) // add our layers after markers have been made  
     neu.addTo(map)
-    let allLayers = L.featureGroup([pos, neg]);
-    map.fitBounds(allLayers.getBounds());
+    let allLayers = L.featureGroup([pos, neg, neu]);
+    // map.fitBounds(allLayers.getBounds());
+    console.log(zipcodes)
+    getBoundary(boundaryLayer, zipcodes)
 }
 
 loadData(dataUrl)
